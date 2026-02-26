fd5a033c0952d96a433a1e3621b76a631f13f034
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-02-26 20:18:22 +0000

    THIS IS GONNA BE THE BEST DAY OF MY LIFE

--- Counter.cs
+++ Counter.cs
@@ -2,7 +2,22 @@
 public class Counter
 {
     public static int Visitors = 100;
-
     // Add your code here
-    
+    public int PrefixIncrement;
+    public int PostfixIncrement;
+    public int PrefixDecrement;
+    public int PostfixDecrement;
+    public Counter()
+    {
+        PrefixIncrement = ++Visitors;
+        PostfixIncrement = Visitors++;
+        PrefixDecrement = --Visitors;
+        PostfixDecrement = Visitors--;
+    }
 }
+
+//should assign the prefix incremented value of Visitors to a class-level variable named PrefixIncrement
+//should assign the postfix incremented value of Visitors to a class-level variable named PostfixIncrement
+//should assign the prefix decrement value of Visitors to a class-level variable named PrefixDecrement
+//should assign the postfix decrement value of Visitors to a class-level variable named PostfixDecrement
+//should have a final value of 100 stored in the variable Visitors
